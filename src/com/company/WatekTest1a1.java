b0911565b70a2f3386c4aeb1aee4fdba115753a1
Krzysztof-kody <53033822+Krzysztof-kody@users.noreply.github.com>
2021-05-10 11:28:22 +0200

    thread.start()

--- src/com/company/WatekTest1a1.java
+++ src/com/company/WatekTest1a1.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class WatekTest1a1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Watek1a1 w1 = new Watek1a1("Ala", 50);
-        w1.run();
-
+        Watek1a1 w1 = new Watek1a1("Ala", 1000);
+        w1.start();
+        Watek1a1 w2 = new Watek1a1("ma", 800);
+        w2.start();
     }
 }
